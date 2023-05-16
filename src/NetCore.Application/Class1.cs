b4d585b572262527120e2734676a477aa4703c6f
Boris Sea <seaborisrc@gmail.com>
2023-05-15 21:40:40 -0400

    Add initial structure of project

---
+++ src/NetCore.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NetCore.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/NetCore.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NetCore.Persistence
+{
+    public class Class1
+    {
+    }
+}
